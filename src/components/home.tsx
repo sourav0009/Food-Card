c7205c3fb622e4dc8d174f962946d5c5ccef20a8
Sourav nandi <118915260+sourav0009@users.noreply.github.com>
2024-09-16 17:20:14 +0530

    change background

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -5,9 +5,9 @@ const Home = () => {
     return (
         <>
             <Navber />
-            <section className="bg-center bg-no-repeat bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/conference.jpg')] bg-gray-700 bg-blend-multiply">
+            <section className="bg-center bg-no-repeat bg-[url('home.jpg')] bg-gray-700 ">
                 <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56 h-screen ">
-                    <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">We invest in the world’s potential</h1>
+                    <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Here is Your Favourites Food </h1>
                     <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
                     <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
                         <a href="/veg" className="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400 ">
@@ -30,3 +30,9 @@ const Home = () => {
 }
 
 export default Home
+
+
+
+
+
+{/* <section className="bg-center bg-no-repeat bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/conference.jpg')] bg-gray-700 bg-blend-multiply"> */}
