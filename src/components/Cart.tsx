25e5835869d4fc9ad4af334e1bcb7adaec43d15a
Sourav nandi <118915260+sourav0009@users.noreply.github.com>
2024-09-17 14:43:02 +0530

    card responsiv

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -4,8 +4,14 @@ const Cart = () => {
   return (
     <>
       <div className="flex flex-col mt-20 items-center">
-        <div className=" border  h-[60vh] w-[40vw] p-6 rounded-lg shadow-2xl bg-slate-300 ">
-            <span className=" flex justify-center text-center text-2xl font-bold  ">My Order</span>
+        <div className=" border h-[60vh] w-[45vh]  lg:h-[60vh] lg:w-[40vw] p-6 rounded-lg shadow-2xl bg-slate-300 ">
+          <span className=" flex justify-center text-center text-2xl font-bold  ">My Order</span>
+          <div className=" absolute bottom-[55vw] lg:bottom-60">
+            <h3>Items : </h3>
+            <h3>Total Amount : </h3>
+            <hr/>
+            <button className=" bg-green-500 font-bold rounded-lg px-3 text-white py-2 w-72 lg:w-[37vw]">Checkout</button>
+          </div>
         </div>
       </div>
     </>
