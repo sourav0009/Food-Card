455684cf82c2b0d2eadaa3d15c6c43c931148013
Sourav nandi <118915260+sourav0009@users.noreply.github.com>
2024-09-24 18:53:03 +0530

    text add

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -3,11 +3,12 @@
 const About = () => {
   return (
     <div className="">
-      <span className=" flex justify-center font-bold text-4xl">About Me</span>
+      <span className=" flex justify-center font-bold text-4xl mt-8">About Me</span>
       <div className=" flex justify-center mt-40 space-x-16">
-        <div className=" h-68 w-[120vh] flex border gap-28" >
-          <p className=" text-center mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti porro fuga non assumenda ipsum quia sint officiis similique? Deleniti maiores ipsam laborum alias iure illum nobis obcaecati quibusdam omnis impedit earum ipsa fuga assumenda qui illo dolores eveniet, dolorum nulla debitis deserunt non recusandae. Vitae culpa optio aliquid distinctio placeat?</p>
-          <img className="h-[50vh] rounded-full"  src="https://scontent.fccu31-2.fna.fbcdn.net/v/t39.30808-6/338715009_741580874121085_3626178987623533266_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=a5f93a&_nc_ohc=tlnisaRrUyIQ7kNvgFNWLgB&_nc_ht=scontent.fccu31-2.fna&_nc_gid=AFe2FtMJ_AOokwtNDfRwutC&oh=00_AYCLf-g6Hvzlv_etmW_5lUCy80BoZ08WJPth7QqRqh0HxQ&oe=66F86202" alt="" />
+        <div className=" h-68 w-[120vh] flex " >
+        {/* <span className=" flex justify-center font-bold text-4xl">About Me</span> */}
+          <p className="  mt-2 font-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti porro fuga non assumenda ipsum quia sint officiis similique? Deleniti maiores ipsam laborum alias iure illum nobis obcaecati quibusdam omnis impedit earum ipsa fuga assumenda qui illo dolores eveniet, dolorum nulla debitis deserunt non recusandae. Vitae culpa optio aliquid distinctio placeat jndfelmsd njfjdslnfjsfnndh msddbfinsds bnbhsfjdfn mnbjvhjernd bdhidrfj bvuhdfgjrde jdfnf uhd hgjdfb hhr fuewo gfvew etew gefwgu  efew?</p>
+          <img className="h-[50vh] rounded-full shadow-2xl"  src="https://scontent.fccu31-2.fna.fbcdn.net/v/t39.30808-6/338715009_741580874121085_3626178987623533266_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=a5f93a&_nc_ohc=tlnisaRrUyIQ7kNvgFNWLgB&_nc_ht=scontent.fccu31-2.fna&_nc_gid=AFe2FtMJ_AOokwtNDfRwutC&oh=00_AYCLf-g6Hvzlv_etmW_5lUCy80BoZ08WJPth7QqRqh0HxQ&oe=66F86202" alt="" />
         </div>
       </div>
     </div>
