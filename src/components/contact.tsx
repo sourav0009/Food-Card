49cbca3ddf1a934668762c366a20212b590cf337
Sourav nandi <118915260+sourav0009@users.noreply.github.com>
2024-09-24 19:39:18 +0530

    div

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -3,9 +3,13 @@ const Contact = () => {
   return (
     <section className="bg-center bg-no-repeat bg-[url('home.jpg')] bg-gray-700 ">
        <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56 h-screen ">
-        
-       </div>
+        <div className="grid place-content-center ">
+          <div className=" h-48 w-64 border">
 
+          </div>
+
+        </div>
+       </div>
     </section>
   )
 }
