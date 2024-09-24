31f74822be76035c12f37df7a450f53fdab45317
Sourav nandi <118915260+sourav0009@users.noreply.github.com>
2024-09-24 20:25:41 +0530

    contact

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -2,12 +2,16 @@
 const Contact = () => {
   return (
     <section className="bg-center bg-no-repeat bg-[url('home.jpg')] bg-gray-700 ">
-       <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56 h-screen ">
-        <div className="grid place-content-center ">
-          <div className=" h-48 w-64 border">
-
+       <div className="px-4 mx-auto max-w-screen-xl py-2 lg:py-56 h-screen ">
+        <div className=" flex place-content-center ">
+          <div className=" h-96 w-[96vh] border">
+            <h1 className=" flex justify-center text-white font-bold text-2xl mt-4">Contact us</h1>
+            <div className=" flex flex-col space-y-6">
+              <input className=" w-14px h-10px" type="text" placeholder="Enter your name" />
+              <input className=" w-14px h-10px" type="text" placeholder="E-Mail" />
+              <input className=" w-14px h-10px" type="text" placeholder="Message" />
+            </div>
           </div>
-
         </div>
        </div>
     </section>
